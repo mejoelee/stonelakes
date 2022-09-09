99025a0a8be8b6048f9eb5c9ac661f6f7d5fa0ed
mejoelee <mejoelee@gmail.com>
2022-09-09 01:27:55 -0400

    added minute math

--- addSubtract.js
+++ addSubtract.js
@@ -16,7 +16,7 @@ function writeTable( pStart, pEnd )
 		document.write( ")'>" );
 		document.write( i );
 		document.write( "</td>\r\n" );
-		if( i % 10 == 0 && i != pEnd )
+		if( i % 10 == 9 && i != pEnd )
 		{
 			document.write( "</tr><tr>\r\n" );	
 		}
@@ -24,49 +24,123 @@ function writeTable( pStart, pEnd )
 	document.write( "</tr></table>\r\n" );
 }
 
-var sCurrentEquation = null;
+var sNumberSet = null;
+var sCurrentIndex = null;
 var sStart = null;
 var sRange = null;
-var sNumberOfQuestions = null;
-var sCorrectCount = null;
-var sAskedCount = null;
+var sIncorrectCount = null;
+var sStartTime = null;
+var sEquations = null;
+var sIsAddition = null;
 
-function startQuiz( pStart, pEnd, pNumberOfQuestions )
-{	
+function startQuiz( pNumberSet, pStart, pEnd, pIsAddition )
+{
+	sNumberSet = pNumberSet;
 	sStart = pStart;
 	sRange = pEnd - pStart;
-	sNumberOfQuestions = pNumberOfQuestions;
-	sAskedCount = 0;
-	sCorrectCount = 0;
+	sIncorrectCount = 0;
+	sStartTime = Date.now();
+	sIsAddition = pIsAddition;
 	
-	askNextQuestion();
+	/**
+		create the questions. the only way to avoid duplicates
+		and omissions is to generate all of the questions
+		sequentially and pick them at random, removing them
+		from the array as we go
+	**/
+	sEquations = new Array();
+	if( sNumberSet == 8888 )
+	{
+		hide( "answers" );
+		get( "equation" ).innerHTML = "";	
+	}
+	else
+	{
+		if( sNumberSet == 9999 )
+		{
+			for( let iFirstNumber = pStart; iFirstNumber <= pEnd; iFirstNumber++ )
+			{
+				createAdditionQuestions( iFirstNumber, pStart, pEnd );
+				createSubtractionQuestions( iFirstNumber, pStart, pEnd );
+			}
+		}
+		else if( pIsAddition )
+		{
+			createAdditionQuestions( pNumberSet, pStart, pEnd );
+		}
+		else
+		{
+			createSubtractionQuestions( pNumberSet, pStart, pEnd );
+		}
+		
+		show( "answers" );
+		askNextQuestion();
+		}
+}
+
+function createAdditionQuestions( pNumberSet, pStart, pEnd )
+{
+	for( let iSecondNumber = pStart; iSecondNumber <= pEnd; iSecondNumber++ )
+	{
+		let lEquation = new Equation( pNumberSet, iSecondNumber, true );
+		sEquations.push( lEquation );
+	}
+}
+
+function createSubtractionQuestions( pNumberSet, pStart, pEnd )
+{
+	for( let i = pStart; i <= pEnd; i++ )
+	{
+		let lFirstNumber = pNumberSet + i;
+		let lEquation = new Equation( lFirstNumber, pNumberSet, false );
+		sEquations.push( lEquation );
+	}
 }
 
 function askNextQuestion()
 {
-	if( sAskedCount++ < sNumberOfQuestions )
+	// are there more questions?
+	if( sEquations.length > 0 )
 	{
-		let lFirstNumber = 0;
-		let lSecondNumber = 0;
-		
-		// pick 2 random numbers
-		do
-		{
-			lFirstNumber = Math.floor( sRange * Math.random() ) + sStart;
-			lSecondNumber = Math.floor( sRange * Math.random() ) + sStart;
-		}
-		while( lFirstNumber == lSecondNumber ); // make sure the number aren't equal
-		
-		// determine the operator
-		let lIsAddition = Math.random() < 0.5;
-		
-		// create and show new equation
-		sCurrentEquation = new Equation( lFirstNumber, lSecondNumber, lIsAddition );
-		sCurrentEquation.show();
+		sCurrentIndex = Math.floor( sEquations.length * Math.random() );
+		sEquations[ sCurrentIndex ].show();
 	}
 	else // done, display results
 	{
-		alert(sCorrectCount);
+		hide( "answers" );
+		const lTime = Math.round(( Date.now() - sStartTime ) / 1000 );
+		if( sIncorrectCount == 0 )
+		{
+			get( "equation" ).innerHTML = "You answered all correctly<br>in " + lTime + " seconds!<br><button onClick='startQuiz( sNumberSet, sStart, sRange + sStart, sIsAddition )'>Try Again</button>"
+		}
+		else
+		{
+			get( "equation" ).innerHTML = "You answered " + sIncorrectCount + " incorrectly<br>in " + lTime + " seconds!<br><button onClick='startQuiz( sNumberSet, sStart, sRange + sStart, sIsAddition )'>Try Again</button>"		
+		}
+	}
+}
+
+var sOperations = [ "+", "-" ];
+function writeSelectNumberSet()
+{
+	// addition
+	for( let i = 2; i <= 10; i++ )
+	{
+		document.write( "<option value='" );
+		document.write( i );
+		document.write( "+'>+" );
+		document.write( i );
+		document.write( "</option>" );
+	}
+	
+	// subtraction
+	for( let i = 0; i <= 10; i++ )
+	{
+		document.write( "<option value='" );
+		document.write( i );
+		document.write( "-'>-" );
+		document.write( i );
+		document.write( "</option>" );
 	}
 }
 
@@ -74,11 +148,8 @@ class Equation
 {
 	constructor( pFirstNumber, pSecondNumber, pIsAddition )
 	{
-		// transpose the numbers if the operation is subtraction and the second number is greater
-		let lTransposeNumbers = !pIsAddition && ( pSecondNumber > pFirstNumber );
-		
-		this.mFirstNumber = lTransposeNumbers ? pSecondNumber : pFirstNumber;
-		this.mSecondNumber = lTransposeNumbers ? pFirstNumber : pSecondNumber;
+		this.mFirstNumber = pFirstNumber;
+		this.mSecondNumber = pSecondNumber;
 		this.mOperator = pIsAddition ? "+" : "-";
 		this.mSumOrDifference = pIsAddition ? pFirstNumber + pSecondNumber : this.mFirstNumber - this.mSecondNumber;
 	}
@@ -106,21 +177,35 @@ function showEquation( pText )
 	get( "equation" ).innerHTML = "<p class='equation'>" + pText + "</p>";
 }
 
+var sWasClicked = false;
 async function clickedAnswer( pNumber )
 {
-	if( pNumber == sCurrentEquation.mSumOrDifference )
+	if( !sWasClicked )
 	{
-		// correct answer was given
-		++sCorrectCount;
-		sCurrentEquation.show( true );
-		playSound( "right" );
-		await sleep( 500 );
+		sWasClicked = true;
+		const lCurrentEquation = sEquations[ sCurrentIndex ];
+		if( pNumber == lCurrentEquation.mSumOrDifference )
+		{
+			// correct answer was given
+			lCurrentEquation.show( true );
+			sEquations.splice( sCurrentIndex, 1 );
+			playSound( "right" );
+			await sleep( 500 );
+		}
+		else // wrong answer
+		{
+			++sIncorrectCount;
+			lCurrentEquation.show( true, true );
+			playSound( "wrong" );
+			await sleep( 2000 );
+		}
+		askNextQuestion();
+		sWasClicked = false;
 	}
-	else // wrong answer
-	{
-		sCurrentEquation.show( true, true );
-		playSound( "wrong" );
-		await sleep( 2000 );
-	}
-	askNextQuestion();
+}
+var sNumberSet = null;
+function selectNumberSet( pNumberSet )
+{
+	const lNumberSet = parseInt( pNumberSet.substring( 0, pNumberSet.length - 1 ));
+	startQuiz( lNumberSet, 0, 10, pNumberSet.endsWith( "+" ));
 }
